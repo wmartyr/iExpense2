70ac9ea73ab930d5f387857d786ead6ffad28a59
Woodrow Martyr <woodrow.martyr@gmail.com>
2025-01-30 06:41:19 +1030

    Initial Commit

---
+++ iExpense2/iExpense2App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense2App.swift
+//  iExpense2
+//
+//  Created by Woodrow Martyr on 30/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpense2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
