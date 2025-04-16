048aadfed0ff1463d681f5b35c33a45fa508607b
Woodrow Martyr <woodrow.martyr@gmail.com>
2025-04-17 07:05:29 +0930

    After Final Polish instructions

---
+++ iExpense2/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense2
+//
+//  Created by Woodrow Martyr on 1/2/2025.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
